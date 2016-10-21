c23587dc792e25444a00eb942883233c6819454b
oxape <wxdeve@126.com>
2016-10-21 13:23:23 +0800

    加入工程文件

---
+++ structure-oc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  structure-oc
+//
+//  Created by oxape on 16/10/21.
+//  Copyright © 2016年 oxape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ structure-oc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  structure-oc
+//
+//  Created by oxape on 16/10/21.
+//  Copyright © 2016年 oxape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
